74ff69e7d3e2e69da89d30309f575d5b9d0266ef
Piyush Anand <anand.pi@northeastern.edu>
2022-07-06 12:30:26 -0400

    updated the page to correct duplicate

--- pages/abhi.py
+++ pages/abhi.py
@@ -12,19 +12,19 @@ import seaborn as sns
 
 
 def app():
-    st.markdown("# Spy Plane Registration Details based on No of flights")
-    st.sidebar.header("No of flights")
+    st.markdown("# Spy Plane Registration Details based on Manufacture year")
+    st.sidebar.header("MFG Records")
 
 
     st.write(
-        """This page gives you the records of Planes with respect to the number of times it took flight """
+        """This page gives you the records of Planes with respect to it's manufacture year and if it's being used for surveillance or not """
     )
 
 
 
-    image ='https://www.popsci.com/uploads/2022/03/11/A380-flight-test-platform-for-the-ZEROe-demonstrator-.jpg?auto=webp&width=1440&height=958.32'
+    image ='https://prd-sc101-cdn.rtx.com/-/media/ca/newsroom/images/2020/mission/u2--news-release.jpg?rev=b2c3dc181bf248c4911d930a20787ffc'
 
-    st.image(image, caption='Aircraft')
+    st.image(image, caption='Surveillance Aircraft')
 
     image1='https://img.buzzfeed.com/buzzfeed-static/static/2017-07/29/11/asset/buzzfeed-prod-fastlane-01/sub-buzz-2347-1501342449-1.png?resize=990:713?output-quality=auto&output-format=auto&downsize=640:*'
 
@@ -34,41 +34,52 @@ def app():
     st.write(
         """A sample of the dataset: """
     )
-    csv=pd.read_csv('https://damg7245assignment.s3.amazonaws.com/planes_features.csv', nrows=5)
+    csv=pd.read_csv('https://damg7245assignment.s3.amazonaws.com/faa_registration.csv', nrows=10)
     df = pd.DataFrame(csv)
     df1=df[:5]
     st.dataframe(df1, width=900, height=200)  # 👈 Draw the dataframe
+    ax = df['YEAR MFR'].value_counts()
+    st.write(
+        """Here is a plot which shows the counts of planes Manufactured in years """
+    )
+    st.bar_chart(ax)
 
 
 
-    url = "https://api-o227c7m3tq-ue.a.run.app/number_of_flights"
+    url = "https://api.anandpiyush.com/number_of_flights"
 
-    quan = st.selectbox(
-        'Do you want to get results for aircrafts which took less than or more than number of flights, 0 indicates less than and 1 indicates more than:',
+
+    n = st.selectbox(
+        'Do you want to get results for surveillance aircrafts or all other aircrafts, 0 indicates surveillance and 1 indicates others',
         (0, 1))
 
 
-    if quan==0: 
-        a='less than' 
+    if n==0: 
+        a='surveil' 
     else:
-        a='more than'
+        a='other'
 
 
     st.write('You selected:', a)
 
 
-    flights = st.slider(
-    'Select the number of flights from the slider',min_value=2, max_value=1000)
+    if n==0:
+        year = st.selectbox(
+        'Select from the dropdown list the year of manufacture of the aircraft',
+        (2001,2003,2004,2005,2008,2009,2010,2011,2012, 2013))
+    else:
+        year = st.select_slider(
+        'Select from the slider list the year of manufacture of the aircraft',options=(1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1988,1989,1990,991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,
+        2006,2007,2008,2009,2010,2011,2012,2013,2014,2015))
+    st.write('You selected:', year)
 
-    st.write('You selected:', flights)
 
 
 
 
-
-    def result(quan,flights):
-        payload={'quan': quan, 'flights' : flights }
-        url = f"https://api.anandpiyush.com/number_of_flights?quantity={payload['quan']}&flights={payload['flights']}"
+    def result(n,year):
+        payload={'n': n, 'year' : year }
+        url = f"https://api.anandpiyush.com/planes_info_from_manufacture_year?surveil={payload['n']}&year={payload['year']}"
         header={}
         header['Authorization'] = f"Bearer {st.session_state['access_token']}"
         response = requests.request("GET", url, params=payload,  headers=header)
@@ -80,15 +91,18 @@ def app():
     buttonstat=st.button('Get Results', disabled=False)
     if buttonstat:
         st.write('The following are the records')
-        df2=result(quan,flights)
+        df2=result(n,year)
         st.dataframe(df2)
-        st.write('Plot of flight code vs number of flights (top 20):')
-        fig2=plt.figure()
-        sns.barplot(x='type', y='flights', data=df2.head(20))
-        plt.xticks(rotation=90)
-        st.pyplot(fig2)
-        st.write('Plot of flight type vs number of flights (top 20):')
-        fig=plt.figure()
-        sns.barplot(x='adshex', y='flights', data=df2.head(20))
-        plt.xticks(rotation=90)
-        st.pyplot(fig)
+        st.markdown("***")
+
+        chart=df2['MFR_MDL_CODE'].value_counts()
+        chart1=df2['ENG_MFR_MDL'].value_counts()
+        st.markdown('Bar chart displaying counts of all manufacture models:')
+
+        st.bar_chart(chart)
+
+        st.markdown("***")
+
+        st.markdown('Bar chart displaying counts of all engine manufacture models:')
+
+        st.bar_chart(chart1)
